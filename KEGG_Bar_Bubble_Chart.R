4f3d81f5ba867b7891e5b2db8bd5f5058d02747a
L. Mingkai <96586384+LamarckLab@users.noreply.github.com>
2024-12-05 20:23:40 +0800

    Update KEGG_Bar_Bubble_Chart.R

--- KEGG_Bar_Bubble_Chart.R
+++ KEGG_Bar_Bubble_Chart.R
@@ -8,7 +8,8 @@ kegg_frame <- read.csv(file_path)
 # 确保 order 是因子类型并按 Count 排序
 kegg_frame$order <- factor(kegg_frame$order, levels = kegg_frame$order[order(kegg_frame$Count)])
 
-ggplot(hh,aes(y=order,x=Count,fill=p.adjust))+
+# 绘制柱状图
+ggplot(kegg_frame,aes(y=order,x=Count,fill=p.adjust))+
   geom_bar(stat = "identity",width=0.7)+####柱子宽度
   #coord_flip()+##颠倒横纵轴
   scale_fill_gradient(low = "red",high ="blue" )+#颜色自己可以换
@@ -20,13 +21,13 @@ ggplot(hh,aes(y=order,x=Count,fill=p.adjust))+
         legend.title = element_text(face = "bold",size = 16))+
   theme_bw()
 
-###气泡图
-hh <- as.data.frame(kk)
-rownames(hh) <- 1:nrow(hh)
-hh$order=factor(rev(as.integer(rownames(hh))),labels = rev(hh$Description))
-ggplot(hh,aes(y=order,x=Count))+
+
+# 绘制气泡图
+kegg_frame$order=factor(rev(as.integer(rownames(kegg_frame))),labels = rev(kegg_frame$Description))
+ggplot(kegg_frame,aes(y=order,x=Count))+
   geom_point(aes(size=Count,color=-1*p.adjust))+# 修改点的大小
   scale_color_gradient(low="green",high = "red")+
   labs(color=expression(p.adjust,size="Count"), 
        x="Gene Number",y="Pathways",title="KEGG Pathway Enrichment")+
   theme_bw()
+
